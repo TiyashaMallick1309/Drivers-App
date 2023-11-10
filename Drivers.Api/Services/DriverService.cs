f1b5d96f0d70b8d0f5bbc8b0174236db082d569b
Tiyasha Mallick <92668444+TiyashaMallick1309@users.noreply.github.com>
2023-11-10 10:42:39 +0530

    Add files via upload

---
+++ Drivers.Api/Services/DriverService.cs
@@ -0,0 +1,29 @@
+﻿using Drivers.Api.Configurations;
+using Drivers.Api.Models;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace Drivers.Api.Services
+{
+    public class DriverService
+    {
+        private readonly IMongoCollection<Driver> _driversCollection;
+
+        public DriverService
+            (IOptions<DatabaseSettings> databaseSettings)
+        {
+            //initialise my mongodb client
+            var mongoClient = new MongoClient(databaseSettings.Value.ConnectionString);
+
+            //Connect to mongoDb database
+            var mongoDb = mongoClient.GetDatabase(databaseSettings.Value.DatabaseName);
+            _driversCollection = mongoDb.GetCollection<Driver>(databaseSettings.Value.CollectionName);
+
+        }
+
+        public async Task<List<Driver>> GetAsync()=>
+            await _driversCollection.Find(_=> true).ToListAsync();
+
+
+    }
+}
